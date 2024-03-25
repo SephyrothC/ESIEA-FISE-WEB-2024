047ec5d0424f1c44e6dd6e97d70c107d463aa14f
Adrian Stephan <97838183+SephyrothC@users.noreply.github.com>
2024-03-25 09:42:17 +0100

    Module Test

---
+++ Module-Test/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Page</title>
+</head>
+<body>
+    <header>
+        <h1>Welcome to my page</h1>
+    </header>
+    <body>
+        <section> 
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum eveniet nisi quas impedit aliquam provident, excepturi corrupti sapiente molestias ab fugiat alias doloremque, consectetur fuga. Consequuntur incidunt alias voluptatibus fugiat.</p>
+        </section>
+        <section class = "section 2">
+            <h2>My Section</h2>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum eveniet nisi quas impedit aliquam provident, excepturi corrupti sapiente molestias ab fugiat alias doloremque, consectetur fuga. Consequuntur incidunt alias voluptatibus fugiat.</p>
+        </section>
+        <section class = "section 3">
+            <h2>My Section</h2>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum eveniet nisi quas impedit aliquam provident, excepturi corrupti sapiente molestias ab fugiat alias doloremque, consectetur fuga. Consequuntur incidunt alias voluptatibus fugiat.</p>
+        </section>
+        <footer>
+            <p> &copy; 2020 My Page</p>
+        </footer>
+    </body>
+    
+</body>
+</html>
